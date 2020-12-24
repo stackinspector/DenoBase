a834d1dae5e186128d61746088082cd0cfc7c376
stackinspector <stackinspector@126.com>
2020-12-24 21:18:33 +0800

    adjust comments on `hash/wasm`

--- hash/wasm.ts
+++ hash/wasm.ts
@@ -1,7 +1,7 @@
 import { createHash, SupportedAlgorithm } from 'std/hash/mod.ts'
 export { hash }
 
-/** a shorthand of std/hash */
+/** a shorthand of `std/hash` (rust wasm) */
 const hash = (bin: Uint8Array | ArrayBuffer, alg: SupportedAlgorithm): ArrayBuffer => {
     const hasher = createHash(alg)
     hasher.update(bin)
