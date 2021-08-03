98dff645daaa27654ab4a1611af2964eeedfda49
stackinspector <stackinspector@126.com>
2021-08-04 04:53:29 +0800

    feat(insert-string): first add

---
+++ insert-string.ts
@@ -0,0 +1,11 @@
+/* TODO enhance performance by using string[] as string builders */
+export const insert = (template: string, content: Map<string, string>) => {
+    let current = template
+    for (const [key, val] of content) {
+        const splited = current.split(key)
+        if (splited.length !== 2) throw new Error(`split error when key=${key}`)
+        const [foo, bar] = splited
+        current = foo + val + bar
+    }
+    return current
+}
