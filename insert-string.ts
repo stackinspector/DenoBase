87674c121b983b1cc52fe9d6e8fdda87fc9baa84
stackinspector <stackinspector@126.com>
2021-12-02 12:16:50 +0800

    fix insert-string

--- insert-string.ts
+++ insert-string.ts
@@ -3,9 +3,11 @@ export const insert = (template: string, content: Map<string, string>) => {
     let current = template
     for (const [key, val] of content) {
         const splited = current.split(key)
-        if (splited.length !== 2) throw new Error(`split error when key=${key}`)
-        const [foo, bar] = splited
-        current = foo + val + bar
+        if (splited.length !== 1) {
+            if (splited.length !== 2) throw new Error(`split error when key=${key}`)
+            const [foo, bar] = splited
+            current = foo + val + bar
+        }
     }
     return current
 }
